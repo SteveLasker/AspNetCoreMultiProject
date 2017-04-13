2a6b48c2a5e0432787a2804a75e2232a42114e64
Steve Lasker <StevenLasker@Hotmail.com>
2017-04-13 06:57:01 -0700

    15.3 Preview Updtes

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections;
-using System.Runtime.InteropServices;
-using System.Text;
 
-namespace Web
+namespace Web.Controllers
 {
     public class HomeController : Controller
     {
@@ -18,28 +15,11 @@ namespace Web
 
         public IActionResult About()
         {
-            // Debugging Information 
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
-            {
-                ViewData["Message"] = "Debugging Info.";
-
-                ViewData["HOSTNAME"] = Environment.GetEnvironmentVariable("COMPUTERNAME") ??
-                                                Environment.GetEnvironmentVariable("HOSTNAME");
-                ViewData["OSARCHITECTURE"] = RuntimeInformation.OSArchitecture;
-                ViewData["OSDESCRIPTION"] = RuntimeInformation.OSDescription;
-                ViewData["PROCESSARCHITECTURE"] = RuntimeInformation.ProcessArchitecture;
-                ViewData["FRAMEWORKDESCRIPTION"] = RuntimeInformation.FrameworkDescription;
-                ViewData["ASPNETCORE_ENVIRONMENT"] = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-                StringBuilder envVars = new StringBuilder();
-                foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
-                    envVars.Append(string.Format("<strong>{0}</strong>:{1}<br \\>", de.Key, de.Value));
-
-                ViewData["ENV_VARS"] = envVars.ToString();
-            }
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
+
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
